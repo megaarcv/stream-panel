0a2377d99c4e80931c9bf5947c3390c5fd045a09
megaarcv <modalmega91@gmail.com>
2026-07-15 22:21:51 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,25 @@
-
+{
+  "name": "stream-panel",
+  "version": "1.0.0",
+  "description": "Professional Stream Panel",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "bcrypt": "^6.0.0",
+    "dotenv": "^17.2.1",
+    "ejs": "^3.1.10",
+    "express": "^5.1.0",
+    "express-session": "^1.18.2",
+    "fluent-ffmpeg": "^2.1.3",
+    "fs-extra": "^11.3.1",
+    "jsonwebtoken": "^9.0.2",
+    "multer": "^2.0.2",
+    "socket.io": "^4.8.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.10"
+  }
+}
