3d6ffab40c89b12e4d0485dfc378e84a6d2ac5af
megaarcv <modalmega91@gmail.com>
2026-07-15 22:38:47 +0700

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const storage = multer.diskStorage({
+
+    destination: function(req,file,cb){
+
+        cb(null,"uploads/videos");
+
+    },
+
+    filename: function(req,file,cb){
+
+        cb(null,Date.now()+path.extname(file.originalname));
+
+    }
+
+});
+
+const upload = multer({
+
+    storage:storage
+
+});
+
+router.get("/",(req,res)=>{
 
     res.render("videos");
+
+});
+
+router.post("/upload",upload.single("video"),(req,res)=>{
+
+    res.redirect("/videos");
 
 });
 
