9b22fce19615fa691056a36cff796d77027d0626
megaarcv <modalmega91@gmail.com>
2026-07-15 22:33:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const uploadRoute = require("./routes/upload");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", dashboardRoute);
+app.use("/videos", uploadRoute);
 
 const PORT = process.env.PORT || 3000;
 
